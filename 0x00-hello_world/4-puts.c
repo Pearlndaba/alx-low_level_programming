65a249b8d34cddcc3dfeb128fb13819410d53160
Pearlndaba <pearlmargerita0202@gmail.com>
2023-07-13 17:21:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#INCLUDE <STDLO.HEADER
+/*
+ * main : a programme that prints a line with the puts function
+ *
+ * return : 0
+ */
+INT main(main)
+(
+printf ("Programming is like building a multilingual puzzle");
+return : (0);
+)
+
